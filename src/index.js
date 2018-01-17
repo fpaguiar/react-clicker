154a72fd91d1f457598c7ed39fd3db8b750e9d6c
Felipe Pimentel de Aguiar <felipe.aguiar@philips.com>
2018-01-16 23:25:28 -0200

    feat: Inital setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//Sample function. Delete later.
+export default function test() {
+	const arr = [1,2,3,4,5];
+
+	const newArr = arr.map(val => {
+		return val * 2;
+	});
+
+	return newArr;
+}
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const config = {
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(jsx|js)$/,
+				exclude: '/node_modules',
+				loader: 'babel-loader'
+			}
+		]
+	}
+};
+
+module.exports = config;
