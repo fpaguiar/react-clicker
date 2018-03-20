253557149057b57527d7e9a2f64d0f9621c8e623
Felipe Pimentel de Aguiar <felipe.aguiar@philips.com>
2018-03-20 17:23:26 -0300

    fix: add html-webpack-plugin

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import Clicker from 'components/Clicker';
+import Clicker from 'components/Clicker/Clicker';
 
 ReactDOM.render(<Clicker />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+const htmlWebpackPluginConfig = new htmlWebpackPlugin({
+	template: './src/index.html',
+	filename: 'index.html',
+	inject: 'body'
+});
 
 const config = {
 	entry: './src/index.jsx',
@@ -22,9 +29,20 @@ const config = {
 				test: /\.jsx?$/,
 				exclude: '/node_modules',
 				loader: 'babel-loader'
+			},
+			{
+				test: /\.css$/,
+				exclude: '/node_modules',
+				loaders: [
+					'style-loader',
+					'css-loader'
+				]
 			}
 		]
-	}
+	},
+	plugins: [
+		htmlWebpackPluginConfig
+	]
 };
 
 module.exports = config;
