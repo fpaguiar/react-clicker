a801bdad6d8d58b25d703525dc8b03e9eafd2375
Felipe Pimentel de Aguiar <felipe.aguiar@philips.com>
2018-03-20 14:59:17 -0300

    config: boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,25 @@
 const path = require('path');
 
 const config = {
-	entry: './src/index.js',
+	entry: './src/index.jsx',
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist')
 	},
+	resolve: {
+		extensions: [
+			'.js',
+			'.jsx',
+		],
+		modules: [
+			'node_modules',
+			path.resolve(__dirname, 'src')
+		]
+	},
 	module: {
 		rules: [
 			{
-				test: /\.(jsx|js)$/,
+				test: /\.jsx?$/,
 				exclude: '/node_modules',
 				loader: 'babel-loader'
 			}
