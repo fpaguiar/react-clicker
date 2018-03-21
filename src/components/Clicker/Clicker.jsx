683892c48705645c1f91a27b2d3e1fcbecb620b6
Felipe Pimentel de Aguiar <felipe.pimentel.aguiar@gmail.com>
2018-03-21 20:42:21 -0300

    feat: add content

--- src/components/Clicker/Clicker.jsx
+++ src/components/Clicker/Clicker.jsx
@@ -5,23 +5,26 @@ class Clicker extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {};
+		this.state = {counter: 0};
 	}
 
 	render() {
 		return (
 			<div className="container">
 				<div className="clicker">
-					<div className="content"></div>
+					<div className="content">{this.state.counter}</div>
 					<div className="menu">
 						<div className="button plus">
 							<i data-feather="plus"></i>
+							<div>+</div>
 						</div>
 						<div className="button minus">
 							<i data-feather="minus"></i>
+							<div>-</div>
 						</div>
 						<div className="button reset">
 							<i data-feather="refresh-ccw"></i>
+							<div>*</div>
 						</div>
 					</div>
 				</div>
