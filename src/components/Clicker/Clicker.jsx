15825b725a402af9819ab4c5c11900092ba0873a
Felipe Pimentel de Aguiar <felipe.aguiar@philips.com>
2018-03-26 16:49:41 -0300

    feat: adjust style

--- src/components/Clicker/Clicker.jsx
+++ src/components/Clicker/Clicker.jsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
+import * as PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { updateCounter } from 'actions';
+import * as Feather from 'feather-icons';
 import './Clicker.css';
 
 class Clicker extends React.Component {
 	constructor(props) {
 		super(props);
+	}
+
+	componentDidMount() {
+		Feather.replace();
 	}
 
 	render() {
@@ -16,15 +22,12 @@ class Clicker extends React.Component {
 					<div className="menu">
 						<div className="button plus" onClick={() => this.props.updateCounter(this.props.counter+1)}>
 							<i data-feather="plus"></i>
-							<div>+</div>
 						</div>
 						<div className="button minus" onClick={() => this.props.updateCounter(this.props.counter-1)}>
 							<i data-feather="minus"></i>
-							<div>-</div>
 						</div>
 						<div className="button reset" onClick={() => this.props.updateCounter(0)}>
 							<i data-feather="refresh-ccw"></i>
-							<div>*</div>
 						</div>
 					</div>
 				</div>
@@ -36,5 +39,10 @@ class Clicker extends React.Component {
 function mapStateToProps({ counter }) {
 	return { counter };
 }
+
+Clicker.propTypes = {
+	counter: PropTypes.number.isRequired,
+	updateCounter: PropTypes.func.isRequired
+};
 
 export default connect(mapStateToProps, { updateCounter })(Clicker);
